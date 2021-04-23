69db93db9084480e79dc65dae122613738057a6f
dipak125 <dgope3359@gmail.com>
2021-04-23 14:29:34 +0530

    home1 commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const Home=()=>{
     return(
         <>
-        <h1>my home page dipak kumar gope. how are you.this is home page</h1>
-        <h2>this is for examkple  of how it words and why are you</h2>
-        <h1>my home page dipak kumar gope. how are you.this is home page</h1>
-        <h2>this is for examkple  of how it words and why are you</h2>
+        <h1>Indusnt page home</h1>
+        
         </>
     )
 }
