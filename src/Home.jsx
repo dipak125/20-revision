19bec9b0340cac89d7ed109a682a68e706f0ccda
dipak125 <dgope3359@gmail.com>
2021-04-23 13:55:39 +0530

    hompage again commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home=()=>{
     return(
+        <>
         <h1>my home page dipak kumar gope. how are you.this is home page</h1>
+        <h2>this is for examkple</h2>
+        </>
     )
 }
 export default Home;
