55b965f2f17df8337dbfea04116cd342cc304681
dipak125 <dgope3359@gmail.com>
2021-04-23 14:55:07 +0530

    hello1 commit

---
+++ src/hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home=()=>{
+    return(
+        <>
+        <h1>React page commit</h1>
+        
+        
+        
+        </>
+    )
+}
+export default Home;
