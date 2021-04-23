a3a31769d5155914f74ed5beacfc921251ce62b5
dipak125 <dgope3359@gmail.com>
2021-04-23 14:19:10 +0530

    16th commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ const Home=()=>{
     return(
         <>
         <h1>my home page dipak kumar gope. how are you.this is home page</h1>
-        <h2>this is for examkple  of how it words</h2>
+        <h2>this is for examkple  of how it words and why are you</h2>
         </>
     )
 }
