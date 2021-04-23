a8d1ba2d6726fe1bd25382c5aa4b660878d24f00
dipak125 <dgope3359@gmail.com>
2021-04-23 14:24:21 +0530

    hom2 commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ const Home=()=>{
         <>
         <h1>my home page dipak kumar gope. how are you.this is home page</h1>
         <h2>this is for examkple  of how it words and why are you</h2>
+        <h1>my home page dipak kumar gope. how are you.this is home page</h1>
+        <h2>this is for examkple  of how it words and why are you</h2>
         </>
     )
 }
