a8d0b4d0fe40241aaa277592fef9cf4e651b9654
dipak125 <dgope3359@gmail.com>
2021-04-23 14:44:27 +0530

    home3 commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ const Home=()=>{
     return(
         <>
         <h1>Indusnt page home.and company page we are making it improve</h1>
+        <h1>Indusnt page home.and company page we are making it improve</h1>
         
         </>
     )
